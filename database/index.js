dee2900d2aca83720d78d306fd5f2046bda5c368
shawnhr <shawnk0818@gmail.com>
2018-03-17 21:31:22 -0700

    Create repo schema

--- database/index.js
+++ database/index.js
@@ -3,14 +3,29 @@ mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
+  id: { type: String, unique: true, dropDups: true },
+  name: String,
+  owner_id: String,
+  avatar_url: String,
+  html_url: String,
+  watchers_count: Number,
+  forks_count: Number
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
+let save = (/* TODO */repo, callback) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+  repo.save((err, repo) => {
+    if (err) {
+      callback(err, null)
+    } else {
+      callback(null, repo)
+    }
+  })
+  
 }
 
 module.exports.save = save;
